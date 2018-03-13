b6e664e16c442c37f0ae5c842c33a770a9539e48
Derek Blankenship <derekblankenship05@gmail.com>
2018-03-12 21:11:30 -0400

    implemented random quote generation and tweet button

--- quote-generator.js
+++ quote-generator.js
@@ -1,7 +1,18 @@
-var newQuote = "THIS IS A NEW QUOTE";
+
+
 var quoteBox = document.getElementById("js-quote-box__quote");
+var tweetButton = document.getElementById("js-quote-box__tweet-me");
+
+var quotes = ['"quote 1" -author', '"quote 2" -author', '"quote 3" -author'];
+var currentQuote = '"Random quote goes here" -Author';
 
 function generateQuote() {
-    quoteBox.innerHTML = newQuote;
+    var randomIndex = Math.floor(Math.random() * (quotes.length));
+    currentQuote = quotes[randomIndex];
+    quoteBox.innerHTML = currentQuote;
+}
+
+function tweetQuote() {
+    tweetButton.href = "https://twitter.com/intent/tweet?text=" + encodeURI(currentQuote);
 }
 
